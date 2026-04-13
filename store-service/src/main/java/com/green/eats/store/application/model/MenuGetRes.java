cd89542d1313fa61fb7a517501fd0129c9972b79
soyoung <ssssy93@icloud.com>
2026-04-13 15:41:17 +0900

    스트림 map

---
+++ store-service/src/main/java/com/green/eats/store/application/model/MenuGetRes.java
@@ -0,0 +1,21 @@
+package com.green.eats.store.application.model;
+
+import com.green.eats.store.entity.Menu;
+import com.green.eats.store.enumcode.EnumMenuCategory;
+
+
+public class MenuGetRes {
+    private Long id;
+    private String name;
+    private Integer price;
+    private Integer stockQuantity;
+    private EnumMenuCategory menuCategory;
+
+    public MenuGetRes(Menu menu) {
+        this.menuCategory = menu.getEnumMenuCategory();
+        this.id = menu.getId();
+        this.name = menu.getName();
+        this.price = menu.getPrice();
+        this.stockQuantity = menu.getStockQuantity();
+    }
+}
